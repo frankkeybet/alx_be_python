1b9524eba65d23ebde7012982418d716938a6093
Frankline <frankkeybet@gmail.com>
2025-11-06 05:09:56 +0300

    updated simple_intrest.py file

--- python_introduction/simple_interest.py
+++ python_introduction/simple_interest.py
@@ -1,6 +1,7 @@
 principal = 1000
 rate = 0.05
 time = 3
-I = principal * rate * time
+interest = principal * rate * time
+# I = principal * rate * time
 
-print("The simple interest is:", I)
+print("The simple interest is:", interest)
