e722c813c09f9d127db886754e99a92620c4185c
Frankline <frankkeybet@gmail.com>
2025-11-06 18:30:40 +0300

    created a command that simulates your projected savings per year

---
+++ python_introduction/finance_calculator.py
@@ -0,0 +1,8 @@
+
+income = input("Enter your monthly income: ")
+expenses = input("Enter your monthly expenses: ")
+monthly_savings = int(income) - int(expenses)
+projected_savings = int(monthly_savings * 12 +(monthly_savings * 0.05 * 12))
+
+print("Your Monthly savings are:", monthly_savings)
+print("Your Projected savings for the year are:", projected_savings)
