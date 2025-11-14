8d4dc81ca3a2177951ecd1aa0119913c2075c538
Frankline <frankkeybet@gmail.com>
2025-11-14 05:26:03 +0300

    Added match_case_calculator.py file

---
+++ control-flow/match_case_calculator.py
@@ -0,0 +1,19 @@
+num1 = int(input("Enter the first number:  "))
+num2 = int(input("Enter the second number: "))
+operation = input("Choose an operation (+, -, *, /): ")
+
+match operation:
+    case '+':
+        result = num1 + num2
+    case '-':
+        result = num1 - num2
+    case '*':
+        result = num1 * num2
+    case '/':
+          
+        result = num1 / num2
+    case _:
+        result = "Invalid operation selected."
+    
+print(f"The result is: {result}")
+        
