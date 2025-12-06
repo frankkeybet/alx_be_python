285140425c332c53988b67fc8e1a691ecc0a0acd
Frankline <frankkeybet@gmail.com>
2025-12-06 14:12:17 +0300

    first commit

--- python_introduction/finance_calculator.py
+++
@@ -1,9 +0,0 @@
-# This is a simple finance calculator that computes monthly savings and projected annual savings with a 5% interest rate.
-
-monthly_income = input("Enter your monthly income: ")
-monthly_expenses = input("Enter your monthly expenses: ")
-monthly_savings = float(monthly_income) - float(monthly_expenses)
-projected_savings = float(monthly_savings * 12 +(monthly_savings * 0.05 * 12))
-
-print("Your Monthly savings are:", monthly_savings)
-print("Your Projected savings for the year are:", projected_savings)
--- python_introduction/future_age_calculator.py
+++
@@ -1,5 +0,0 @@
-
-name = input("How old are you? ")
-age = int(name)
-future_age = age + 27
-print("In 2050, you will be", future_age , "years old.")
